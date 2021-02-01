6a6a8a9d9dc2bc1cc53005a16c47e47e34c40c4f
Admunzi <72613258+Admunzi@users.noreply.github.com>
2021-02-01 15:12:55 +0100

    Update Fecha.java

--- Tanda1/Fecha.java
+++ Tanda1/Fecha.java
@@ -10,22 +10,22 @@ package Tanda1;
 
  * Fecha f = Fecha(1, 10, 2020);
 
- * donde los par·metros que se le pasan al constructor son el dÌa, el mes y el aÒo 
+ * donde los par√°metros que se le pasan al constructor son el d√≠a, el mes y el a√±o 
  * respectivamente. Si la fecha creada es incorrecta mostraremos un mensaje de error 
- * en la salida est·ndar de errores (lo ideal serÌa lanzar una excepciÛn, ya lo 
+ * en la salida est√°ndar de errores (lo ideal ser√≠a lanzar una excepci√≥n, ya lo 
  * haremos cuando se vea en clase).
 
- * Crea mÈtodos para:
+ * Crea m√©todos para:
 
  * Saber si la fecha almacenada es correcta.
- * Sumar y restar dÌas a la fecha. 
- * Comparar la fecha almacenada con otra, el mÈtodo devolver· un valor negativo si la fecha almacenada es ANTERIOR a la otra, 0 si son IGUALES y un valor positivo si es POSTERIOR.
- * Saber si el aÒo de la fecha almacenada es bisiesto.
- * El mÈtodo toString() devuelve una cadena con el formato "<dÌa del mes> de <nombre 
- * del mes> de <aÒo>".
- * Las opciones a partir de la segunda, solo se realizar·n si la fecha almacenada es 
- * correcta, en caso de no serlo, se dar· un aviso, por la salida est·ndar de errores, 
- * de que la operaciÛn no se puede realizar.
+ * Sumar y restar d√≠as a la fecha. 
+ * Comparar la fecha almacenada con otra, el m√©todo devolver√° un valor negativo si la fecha almacenada es ANTERIOR a la otra, 0 si son IGUALES y un valor positivo si es POSTERIOR.
+ * Saber si el a√±o de la fecha almacenada es bisiesto.
+ * El m√©todo toString() devuelve una cadena con el formato "<d√≠a del mes> de <nombre 
+ * del mes> de <a√±o>".
+ * Las opciones a partir de la segunda, solo se realizar√°n si la fecha almacenada es 
+ * correcta, en caso de no serlo, se dar√° un aviso, por la salida est√°ndar de errores, 
+ * de que la operaci√≥n no se puede realizar.
  * @author xulog
  *
  */
@@ -124,7 +124,7 @@ public class Fecha {
       // Compruevo si el dia es mas grande que el mes
       this.setDia((this.dia + n) - diasDelMes[this.mes]);
       // Sumamos al mes uno y comprobamos si el mes es mayor que 13 para ponerlo enero y sumar uno
-      // el aÒo
+      // el a√±o
 
       this.setMes(getMes() + 1);
       if (this.mes >= 13) {
@@ -142,8 +142,8 @@ public class Fecha {
   public Fecha restarDia(int n) {
     // Comprobamos si es menor que 1
     if ((this.dia - n) < 1) {
-      // restamos al mes 1 y comprobamos si el mes va a menos que enero y si es asÌ retrocedemos el
-      // aÒo y ponemos diciembre
+      // restamos al mes 1 y comprobamos si el mes va a menos que enero y si es as√≠ retrocedemos el
+      // a√±o y ponemos diciembre
       this.setDia(diasDelMes[this.mes] - (this.dia - n));
       this.setMes(this.getMes() - 1);
       if (this.mes < 1) {
@@ -158,21 +158,7 @@ public class Fecha {
     }
   }
 
-  /*
-  // Comparamos fechas
-  public int compareTo(Fecha another) {
-    if (this.compareTo(another) > 0) {
-      System.out.println("mayor");
-      return 1;
-    } else if (this.compareTo(another) < 0) {
-      System.out.println("menor");
-      return -1;
-    } else {
-      System.out.println("igual");
-      return 0;
-    }
-  }*/
-  
+  //Comprobamos si el anio es bisiesto
   public boolean esBisiesto() {
     if (this.anio % 100 == 0 && this.anio % 400 == 0) {
       diasDelMes [1] = 29;  
